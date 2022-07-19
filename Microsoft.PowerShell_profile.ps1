cc0933c0a8cf95ee8e68cd30fd507aae2930ea4b
xsy420 <1259795637@qq.com>
2022-07-20 02:00:28 +0800

    add powershell config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module oh-my-posh
+oh-my-posh prompt init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/mt.omp.json | Invoke-Expression
